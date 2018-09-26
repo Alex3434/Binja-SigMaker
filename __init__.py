6229f2e076ffb772813400ed67e982d9e3f7ddac
Alexander Jeuk <alexander2.jeuk@t-online.de>
2018-09-26 23:17:55 +0200

    goto option

--- __init__.py
+++ __init__.py
@@ -1,27 +1,59 @@
 from binaryninja import *
+from multiprocessing import *
 
 def get_address_from_sig(bv, sigList):
 	br = BinaryReader(bv)
 
 	result = 0
 
+	length = len(sigList) - 1
+
 	for search_func in bv.functions:
 		br.seek(search_func.start)
 
-		while bv.get_functions_containing(br.offset) != None and search_func in bv.get_functions_containing(br.offset):
+		while bv.get_functions_containing(br.offset + length) != None and search_func in bv.get_functions_containing(br.offset + length):
 			found = True
+			counter = 0
 			for entry in sigList:
 				byte = br.read8()
+				counter += 1
 				if entry != byte and entry != '?':
 					found = False
 					break
+
+			br.offset -= counter
+				
 			if found:
 				result = br.offset
 				break
+
+			br.offset += bv.get_instruction_length(br.offset)
+			
 		if result != 0:
 			break
 
 	return result
+
+def test_address_for_sig(bv, addr, sigList):
+	br = BinaryReader(bv)
+
+	length = len(sigList) - 1
+
+	br.seek(addr)
+
+	containing = bv.get_functions_containing(br.offset + length)
+
+	if containing == None or len(containing) == 0:
+		return False
+	
+	found = True
+	for entry in sigList:
+		byte = br.read8()
+		if entry != byte and entry != '?':
+			found = False
+			break
+
+	return found
 
 def get_amount_of_hits(bv, sigList):
 	br = BinaryReader(bv)
@@ -31,21 +63,133 @@ def get_amount_of_hits(bv, sigList):
 	if len(sigList) == 0:
 		return result
 
-	if len(sigList) < 5:
-		return -1
+	#if len(sigList) < 3:
+	#	return -1
+
+	sigLen = len(sigList) - 1
 
 	for search_func in bv.functions:
 		br.seek(search_func.start)
 
-		while bv.get_functions_containing(br.offset) != None and search_func in bv.get_functions_containing(br.offset):
+		while bv.get_functions_containing(br.offset + sigLen) != None and search_func in bv.get_functions_containing(br.offset + sigLen):
 			found = True
+			counter = 0
 			for entry in sigList:
 				byte = br.read8()
+				counter += 1
 				if entry != byte and entry != '?':
 					found = False
 					break
+
+			br.offset -= counter
+		
 			if found:
 				result += 1
+
+			br.offset += bv.get_instruction_length(br.offset)
+
+	return result
+
+def map_check_functions_for_hits(func_sig_tuple):
+	
+	if func_sig_tuple == None:
+		return []
+	
+	search_func = func_sig_tuple[0]
+	sigList = func_sig_tuple[1]
+	#br = func_sig_tuple[2]
+	bv = func_sig_tuple[2]
+
+	br = BinaryReader(bv)
+
+	br.seek(search_func.start)
+	sigLen = len(sigList) - 1
+
+	result = []
+
+	if sigList == None or bv.get_functions_containing(br.offset + sigLen) == None:
+		return result
+
+	while bv.get_functions_containing(br.offset + sigLen) != None and search_func in bv.get_functions_containing(br.offset + sigLen):
+		found = True
+		counter = 0
+		for entry in sigList:
+			byte = br.read8()
+			counter += 1
+			if entry != byte and entry != '?':
+				found = False
+				break
+
+		br.offset -= counter
+		
+		if found:
+			result.append(br.offset)
+
+		br.offset += bv.get_instruction_length(br.offset)
+
+		if bv.get_instruction_length(br.offset) == 0:
+			break
+
+
+	return result
+
+
+def get_addr_of_hits(bv, sigList):
+	br = BinaryReader(bv)
+
+	result = []
+
+	if len(sigList) == 0:
+		return result
+
+	sigLen = len(sigList) - 1
+
+	#map_func_sig_List = []
+
+	#for func in bv.functions:
+	#	map_func_sig_List.append((func, sigList, bv))
+
+	
+	
+	#Process(target=map_check_functions_for_hits, args=(map_func_sig_List,))
+
+	#p = Pool(processes=8) 
+	#map_result = map(map_check_functions_for_hits, map_func_sig_List)
+
+	#for x in map_result:
+	#	if len(x) == 0:
+	#		continue
+	#	for addr in x:
+	#		result.append(addr)
+
+
+
+	for search_func in bv.functions:
+		br.seek(search_func.start)
+	
+		while bv.get_functions_containing(br.offset + sigLen) != None and search_func in bv.get_functions_containing(br.offset + sigLen):
+			found = True
+			counter = 0
+			for entry in sigList:
+				byte = br.read8()
+				counter += 1
+				if entry != byte and entry != '?':
+					found = False
+					break
+	
+			br.offset -= counter
+		
+			if found:
+				result.append(br.offset)
+	
+			br.offset += bv.get_instruction_length(br.offset)
+	
+			if bv.get_instruction_length(br.offset) == 0:
+				break
+
+			
+
+			
 
 	return result
 
@@ -68,59 +212,101 @@ def SigMakerFind(bv):
 	result = get_address_from_sig(bv, sigList)
 
 	if result != 0:
-		new_result = result - len(sigList)
-		print 'Found:\t' + convert_to_hex_string(new_result) + '\nInside:\t' + convert_to_hex_string(bv.get_functions_containing(new_result)[0].start) + '\nSignature:\t' + user_input + '\nHits:\t' + convert_to_hex_string(get_amount_of_hits(bv,sigList))
-		show_message_box("Search result",'Address:\t' + convert_to_hex_string(new_result) + '\n' + 'Function:\t' + convert_to_hex_string(bv.get_functions_containing(new_result)[0].start) + '\n', MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.InformationIcon)
+		new_result = result
+		print 'Found:\t' + convert_to_hex_string(new_result) + '\nInside:\t' + convert_to_hex_string(bv.get_functions_containing(new_result)[0].start) + '\nSignature:\t' + user_input #+ '\nHits:\t' + convert_to_hex_string(get_amount_of_hits(bv,sigList))
+		res = show_message_box("Search result",'Address:\t' + convert_to_hex_string(new_result) + '\n' + 'Function:\t' + convert_to_hex_string(bv.get_functions_containing(new_result)[0].start) + '\nWant to jump to the address?', MessageBoxButtonSet.YesNoButtonSet, MessageBoxIcon.InformationIcon)
+		if res == MessageBoxButtonResult.YesButton:
+			bv.file.navigate(bv.file.view, new_result)
 	else:
 		print 'Found:\t' + 'None' + '\nInside:\t' + 'None' + '\nSignature:\t' + user_input
 		show_message_box("Search result",'Address:\t' + 'NONE' + '\n' + 'Function:\t' + 'NONE' + '\n', MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.InformationIcon)
 
 	return
 
-def get_sig_from_address(bv, addr, first_try = True):
-	
-	if addr == None:
-		return
+def get_instruction_sig(bv, func, addr):
+	const = func.get_constants_referenced_by(addr)
+	length = bv.get_instruction_length(addr)
 
 	br = BinaryReader(bv)
 
-	sigList = []
-
 	br.seek(addr)
 
-	org_func = bv.get_functions_containing(br.offset)
+	sig = []
 
-	if org_func == None:
+	if len(const) == 0:
+		for x in range(length):
+			sig.append(br.read8())
+	elif len(const) > 0:
+		br.offset += length
+		new_delta = 0
+		for cur_const in const:
+			if cur_const.pointer:
+				new_delta += 4
+			else:
+				br.offset -= new_delta + 1
+				if const[0].value == br.read8():
+					new_delta += 1
+				else:
+					br.offset -= new_delta + 4
+					if const[0].value == br.read32():
+						new_delta += 4
+
+		br.offset = addr
+		for x in range(length - new_delta):
+			sig.append(br.read8())
+		for x in range(new_delta):
+			sig.append('?')
+
+		
+	return sig
+
+def get_sig_from_address(bv, addr, first_try = True):
+	
+	sigList = []
+
+	length = len(sigList) - 1
+
+	if addr == None:
 		return sigList
 
-	while get_amount_of_hits(bv,sigList) != 1:
-		
-		if len(sigList) > 24 and first_try:
+	offset = addr
+
+	org_func = bv.get_functions_containing(offset)
+
+	if len(org_func) == 0:
+		return sigList
+
+	sigList.extend(get_instruction_sig(bv, org_func[0], offset))
+
+	if len([x for x in sigList if x != '?']) < 4:
+		offset += bv.get_instruction_length(offset)
+		sigList.extend(get_instruction_sig(bv, org_func[0], offset))
+
+	hitList = get_addr_of_hits(bv, sigList)
+
+	while len(hitList) > 1:
+		offset += bv.get_instruction_length(offset)
+
+		containing = bv.get_functions_containing(offset + 1)
+
+		if  containing == None or len(containing) == 0 or containing[0] != org_func[0] and first_try:
 			return get_sig_from_address(bv, org_func[0].start, False)
 		elif not first_try:
-			return sigList
-		
-		containing = bv.get_functions_containing(br.offset)
+			return []
 
-		if (containing == None or containing[0] != org_func[0]) and first_try:
+		if len(sigList) > 48 and first_try:
 			return get_sig_from_address(bv, org_func[0].start, False)
 		elif not first_try:
-			return sigList
+			return []
 
-		target_func = containing[0]
-		constants = target_func.get_constants_referenced_by(br.offset)
+		sigList.extend(get_instruction_sig(bv, org_func[0], offset))
 
-		if len(constants) != 0 and constants[0].pointer:
-			length = bv.get_instruction_length(br.offset) - 4
-			for x in range(length):
-				sigList.append(br.read8())
-			for x in range(4):
-				sigList.append('?')
-				br.offset += 1
-		else:
-			length = bv.get_instruction_length(br.offset)
-			for x in range(length):
-				sigList.append(br.read8())
+		for hit in hitList:
+			if hit == addr:
+				continue
+			if not test_address_for_sig(bv, hit, sigList):
+				hitList = [x for x in hitList if x != hit]
+
 
 	return sigList		
 	
@@ -150,15 +336,26 @@ def convert_to_string(sigList):
 	return str_sig
 
 def SigMakerCreate(bv, addr):
+	show_message_box("Create Signature","It can take a while for the plugin to finish.\nThe search will run in the background but you can cancel it at any time.\nPress 'OK' if you want to start.", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.InformationIcon)
 
-	show_message_box("Create Signature","It can take a while for the plugin to finish.\nPress 'OK' if you want to start.", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.InformationIcon)
+	c = Creator(addr, bv)
+	c.start()
 
-	sigList = get_sig_from_address(bv, addr)
 
-	str_sig = convert_to_string(sigList)
-	print 'Created Signature:\t' + str_sig
-	show_message_box("Created Signature",'Address:\t' + convert_to_hex_string(get_address_from_sig(bv, sigList) - len(sigList)) + '\n' + 'Signature:\t' + str_sig + '\n', MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.InformationIcon)
+	
 
+class Creator(BackgroundTaskThread):
+	def __init__(self, addr, bv):
+		BackgroundTaskThread.__init__(self, "Creating Signature...", True)
+		self.addr = addr
+		self.bv = bv
+
+	def run(self):
+		sigList = get_sig_from_address(self.bv, self.addr)
+
+		str_sig = convert_to_string(sigList)
+		print 'Created Signature:\t' + str_sig
+		show_message_box("Created Signature",'Address:\t' + convert_to_hex_string(get_address_from_sig(self.bv, sigList)) + '\n' + 'Signature:\t' + str_sig + '\n', MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.InformationIcon)
 
 
 PluginCommand.register("[SigMaker] Find Signature", "", SigMakerFind)
